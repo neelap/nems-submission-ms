80b65176d00c55b868d09fa51d8249404af34666
neelap <neela_reddy@outlook.com>
2016-05-02 10:51:56 +0530

    commit

---
+++ src/main/java/com/nems/ctx/ms/submission/client/SubmissionClientController.java
@@ -0,0 +1,28 @@
+package com.nems.ctx.ms.submission.client;
+
+import com.nems.ctx.ms.submission.domain.Submission;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by NE281900 on 5/2/2016.
+ */
+@RestController
+public class SubmissionClientController {
+
+    @Autowired SubmissionClient submissionClient;
+
+    @RequestMapping("/sub")
+    public Iterable<Submission> getSubmissions() {
+        return submissionClient.getSubmissions();
+    }
+
+    @FeignClient("get-submissions")
+    interface SubmissionClient {
+        @RequestMapping(value = "/sub", method = RequestMethod.GET)
+        public Iterable<Submission> getSubmissions();
+    }
+}
--- src/main/java/com/nems/ctx/ms/submission/controller/SubmissionController.java
+++ src/main/java/com/nems/ctx/ms/submission/controller/SubmissionController.java
@@ -3,6 +3,8 @@ package com.nems.ctx.ms.submission.controller;
 import com.nems.ctx.ms.submission.domain.Submission;
 import com.nems.ctx.ms.submission.repository.SubmissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SubmissionController {
     @Autowired
+    DiscoveryClient client;
+
+    @Autowired
     SubmissionRepository submissionRepository;
 
     @RequestMapping("/submissions")
     public Iterable<Submission> getSubmissions() {
+        ServiceInstance localInstance = client.getLocalServiceInstance();
+        System.out.println(localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort());
         return submissionRepository.findAll();
     }
 
