2a4676332b877b98a50537763e3c92cf7052b369
neelap <neela_reddy@outlook.com>
2016-05-02 16:09:21 +0530

    commit

--- src/main/java/com/nems/ctx/ms/submission/NemsGetSubmissionsApplication.java
+++ src/main/java/com/nems/ctx/ms/submission/NemsGetSubmissionsApplication.java
@@ -8,8 +8,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
@@ -17,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients
 @EnableHystrix
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class NemsGetSubmissionsApplication {
 
 	public static void main(String[] args) {
