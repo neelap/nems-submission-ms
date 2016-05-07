698f27ede379b981ab000ca45b44124634b3c1ef
neelap <neela_reddy@outlook.com>
2016-05-07 23:59:39 +0530

    commit

--- src/main/java/com/nems/ctx/ms/submission/client/SubmissionClientController.java
+++ src/main/java/com/nems/ctx/ms/submission/client/SubmissionClientController.java
@@ -3,6 +3,7 @@ package com.nems.ctx.ms.submission.client;
 import com.nems.ctx.ms.submission.domain.Submission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,21 @@ public class SubmissionClientController {
 
     @RequestMapping("/sub")
     public Iterable<Submission> getSubmissions() {
+
         return submissionClient.getSubmissions();
+    }
+
+    @RequestMapping(value = "/sub", method= RequestMethod.POST)
+    public Submission saveSubmission(@RequestBody Submission submission){
+      return submissionClient.saveSubmission(submission);
     }
 
     @FeignClient("get-submissions")
     interface SubmissionClient {
         @RequestMapping(value = "/submissions", method = RequestMethod.GET)
         public Iterable<Submission> getSubmissions();
+
+        @RequestMapping(value = "/submissions", method= RequestMethod.POST)
+        public Submission saveSubmission(@RequestBody Submission submission);
     }
 }
